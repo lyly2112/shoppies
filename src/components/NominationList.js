1d39f6ad807e69f1d1e412c5d6b6f281a75276ce
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-05-31 23:50:57 -0700

    Remove heart image from NomonationList component.

--- src/components/NominationList.js
+++ src/components/NominationList.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Nomination from "./Nomination";
-import heart from "../images/heart.svg";
+
 
 function NominationList(counter) {
   return <Nomination counter={counter} />;
