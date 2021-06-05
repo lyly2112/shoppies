cdb22dfab3165c3f4f57253487e61d892de04941
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-06-05 00:39:23 -0700

    Format code in Nomination component.

--- src/components/Nomination.js
+++ src/components/Nomination.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import '../css/font.css';
-import '../css/nominate-button.css';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Favorite from '@material-ui/icons/Favorite';
-import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import React, { useState } from "react";
+import "../css/font.css";
+import "../css/nominate-button.css";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Favorite from "@material-ui/icons/Favorite";
+import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
 
 function Nomination(movie) {
   const [counter, setCounter] = useState(0);
@@ -24,17 +24,19 @@ function Nomination(movie) {
         onClick={increment}
         value='counter'
       >
-        	&#9829; &#9825; 
-      </button>*/}
-      
+            </button>*/}
       <FormControlLabel
-        control={<Checkbox icon={<FavoriteBorder />} 
-                  checkedIcon={<Favorite />}
-          name='checkedH' />}
-        label='Nominate'
+        control={
+          <Checkbox
+            icon={<FavoriteBorder />}
+            checkedIcon={<Favorite />}
+            name="checkedH"
+          />
+        }
+        label="Nominate"
       />
-
-      </>
+      counter {counter}
+    </>
   );
 }
 
