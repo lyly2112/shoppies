9de720bb51a2c1dacf97bd2ae5e7ec8d3c7b0194
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-06-11 00:32:56 -0700

    Change RemoveButton compontn style.

--- src/components/RemoveButton.js
+++ src/components/RemoveButton.js
@@ -1,45 +1,33 @@
 import React, { useState, useEffect } from "react";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Button from "@material-ui/core/Button";
-import Clear from "@material-ui/icons/Clear";
 
+import Clear from "@material-ui/icons/Clear";
 
 import { ListItemSecondaryAction, MuiThemeProvider } from "@material-ui/core";
 
 function RemoveButton() {
   const [count, setCount] = useState(1);
-  const [nominations, setNominations] = useState([])
+  const [nominations, setNominations] = useState([]);
 
   useEffect(() => {
     console.log(count);
   }, [count]);
 
   useEffect(() => {
-setNominations()
-
+    setNominations();
 
     console.log(nominations);
   }, [nominations]);
 
   const removeNomination = (event) => {
-    
-      console.log("movie is removed!!!")
-      setCount(0)
-      
-    
+    console.log("movie is removed!!!");
+    setCount(0);
   };
   return (
     <>
-      <FormControlLabel
-        control={
-          <Button
-            icon={<Clear />}
-
-            onClick={removeNomination}
-          />
-        }
-        label="Remove"
-      />
+    <Button onClick={removeNomination}>{<Clear />} </Button>
+  
     </>
   );
 }
