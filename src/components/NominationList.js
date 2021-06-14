ccc8f9b7344dd3392677fddf8209641adad4de92
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-06-14 15:18:19 -0700

    Work on NomincationList component.

--- src/components/NominationList.js
+++ src/components/NominationList.js
@@ -1,16 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
+// import { useState } from 'react';
+import { useEffect } from 'react';
 import Nomination from "./Nomination";
 
-function NominationList(props) {
+function NominationList({ nominations, count }) {
+  let nominationsFromLocalStorage = localStorage.getItem('nominations');
+
+  useEffect(() => {
+    console.log("nominations from localStorage: ", nominationsFromLocalStorage[0]);
+  }, [nominationsFromLocalStorage]);
+
   return (
-    <ul>
-      {/* {nominations.map((nominations) => (
-        <Nomination key={nominations.imdbID} {...nominations} />
-      ))} */}
-      {props.nominations.map((nominations, index) => {
-        return <Nomination nominations={nominations}  index={index}/>;
-      })}
-    </ul>
+    // {props.nominations.map((nomination, count) => {
+    //   return (
+    //     <Nomination
+    //       nomination={nomination}
+    //       key={nomination.imdbID}
+    //       count={count}
+    //     />
+    //   );
+    // })}
+
+    <div>
+      <div>NOMINATIONS</div>
+      <div className="results-container__list">
+        <ul>
+          {nominations &&
+            nominations.map((nomination) => {
+              // {console.log('value.Title =>>>', movie.Title);}
+              return <Nomination nomination={nomination} count={count} />;
+            })}
+        </ul>
+      </div>
+    </div>
   );
 }
 export default NominationList;
