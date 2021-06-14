66bbc33ae6f6ebed1ce9946444c6ad454ce34b05
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-06-14 15:11:37 -0700

    Create Nominate component.

---
+++ src/components/Nominate.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Favorite from "@material-ui/icons/Favorite";
+import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
+// import NominationList from "./NominationList";
+// import Nomination from "./Nomination";
+
+// import { ListItemSecondaryAction, MuiThemeProvider } from "@material-ui/core";
+
+function Nominate({ movie }) {
+  const [count, setCount] = useState(0);
+  const [nominations, setNominations] = useState([]);
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
+  // useEffect(() => {
+  //   console.log("nominations: ", movie);
+  // }, [nominations]);
+
+  const handleChange = (event) => {
+    if (event.target.checked) {
+      console.log("heart is checked!!!");
+      setCount(1);
+      setNominations({
+        ...movie,
+        [event.target.movie]: event.target.checked,
+      });
+
+      // setNominations({
+      //   ...movie,
+      //   nominations,
+      // });
+      // let nominations = []
+      // console.log('here ----->>>', localStorage.getItem('nominations'));
+      // nominations.push(movie)
+      // localStorage.setItem('nominations', nominations);
+
+      // <NominationList nominations={nominations} count={count}/>;
+    }
+  };
+
+  return (
+    <>
+      <FormControlLabel
+        control={
+          <Checkbox
+            icon={<FavoriteBorder />}
+            checkedIcon={
+              <Favorite
+                style={{
+                  color: "#FF0000",
+                }}
+              />
+            }
+            //name="checkedH"
+          />
+        }
+        onChange={handleChange}
+        //label="Nominate"
+      />
+    </>
+  );
+}
+
+export default Nominate;
