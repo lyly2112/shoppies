b532ddc3cf018b30f98fa20b687fd21fc4f0b9a8
lyly2112 <31433730+lyly2112@users.noreply.github.com>
2021-06-21 23:39:24 -0700

    Add remove nomination button to the App component.

--- src/App.js
+++ src/App.js
@@ -6,54 +6,67 @@ import "./App.css";
 import Search from "./components/Search";
 // import NominationList from "./components/NominationList";
 // import RemoveButton from "./components/RemoveButton";
+import Clear from "@material-ui/icons/Clear";
 
 function App() {
   const [nominations, setNominations] = useState([]);
-  const [list, setList] = useState(nominations);
+  // const [list, setList] = useState(nominations);
 
-  function handleRemove(imdbID) {
+  function handleRemove(nomination) {
     //remove nomination
-    console.log("nomination.imdbID is ", imdbID);
-    console.log("nominations  list is  ", list);
+    console.log("nomination is ", nomination);
+    {
+      let array = [...nominations]; // make a separate copy of the array
+      console.log('array',array)
+      let index = array.indexOf(nomination);
+      console.log('event.target.value',nomination)
 
-    // const newList = list.filter((nomination) => nomination.imdbID !== imdbID);
-    // setList(newList);
-    console.log("nominations new list is  ", list);
+      console.log('index',index)
+      if (index !== -1) {
+        array.splice(index, 1);
+        setNominations( array );
+      }
+    }
   }
-  useEffect(() => {
-    console.log("App component -> nominations: ", nominations);
-  }, [nominations]);
+  // useEffect(() => {
+  //   console.log("App component -> nominations: ", nominations);
+  // }, [nominations]);
 
-  const test = () => {
-    console.log("Test: App component printing nominations", nominations);
-  };
+  // const test = () => {
+  //   console.log("Test: App component printing nominations", nominations);
+  // };
 
   return (
     <div className="App">
       <Search nominations={nominations} setNominations={setNominations} />
-      <div>NOMINATIONS</div>
+      <div className="results-container__title">Nominations</div>
       <button onClick={() => test()}>Test</button>
       <div className="results-container__list">
-        <ul>
+        <ul className="nominations-container__item">
           {nominations &&
             nominations.map((nomination) => (
-              // {console.log('value.Title =>>>', movie.Title);}
-              // return <Nomination nomination={nomination} />;
-              <li key={nomination.imdbID}>
-                {/* <span>test</span> */}
-                {/* <span>{nomination.imdbID}</span> */}
-                <span>{nomination.Title}</span>
-                <span>{nomination.Year}</span>
-                <img
-                  src={nomination.Poster}
-                  className="result-container_poster"
-                  alt="Poster"
-                />
+              <li className="result-container__movie" key={nomination.imdbID}>
+                <div className="result-container_image">
+                  <img
+                    src={nomination.Poster}
+                    className="result-container_poster"
+                    alt="Poster"
+                  />
+                </div>
+                <div className="result-container_text">
+                  <span className="result-container__title">
+                    {nomination.Title}
+                  </span>
+                  <span className="result-container__year">
+                    {nomination.Year}
+                  </span>
+                </div>
+                
                 <button
                   type="button"
-                  onClick={() => handleRemove(nomination.imdbID)}
+                  onClick={() => handleRemove(nomination)}
                 >
-                  Remove
+                  {<Clear />}Remove
                 </button>
                 {/* <RemoveButton /> */}
               </li>
---
+++ src/components/RemoveButton.js
@@ -0,0 +1,25 @@
+// import React, { useState, useEffect } from "react";
+// // import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import Button from "@material-ui/core/Button";
+
+// import Clear from "@material-ui/icons/Clear";
+
+// // import { ListItemSecondaryAction, MuiThemeProvider } from "@material-ui/core";
+
+// function RemoveButton({ nominations, setNominations }) {
+//   const removeNomination = (event) => {
+//     if (event.target.onClick) {
+//       const newList = list.filter((nomination) => nomination.imdbID !== imdbID);
+//       setNominations(newList);
+//       console.log("nominations new list is  ", newList);
+
+//       console.log(
+//         "Nominate component -> nominations after setNominations remove button ",
+//         nominations
+//       );
+//     }
+//   };
+//   return <Button onClick={removeNomination(nomination.imdbID)}>{<Clear />} </Button>;
+// }
+
+// export default RemoveButton;
